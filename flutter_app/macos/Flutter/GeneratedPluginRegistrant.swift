e243256cbde7dca8eabda12319a0e0e6dab8a5cc
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-07-11 06:59:22 +0700

    resolve https://github.com/flutter/slideplayer/pull/8#discussion_r302305674 check-in all the change as a result of running `flutter run`

--- flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,6 +4,12 @@
 import Foundation
 import FlutterMacOS
 
+import color_panel
+import file_chooser
+import menubar
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ColorPanelPlugin.register(with: registry.registrar(forPlugin: "ColorPanelPlugin"))
+  FileChooserPlugin.register(with: registry.registrar(forPlugin: "FileChooserPlugin"))
+  MenubarPlugin.register(with: registry.registrar(forPlugin: "MenubarPlugin"))
 }
